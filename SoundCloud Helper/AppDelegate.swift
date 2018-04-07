ef13300255778ff1d97ec03a90e4c7a786fcc48c
Worth Baker <wbaker@housecanary.com>
2018-04-07 09:04:56 -0600

    Initial Commit

---
+++ SoundCloud Helper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SoundCloud Helper
+//
+//  Created by Worth Baker on 4/7/18.
+//  Copyright © 2018 worthbak. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SoundCloud Helper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SoundCloud Helper
+//
+//  Created by Worth Baker on 4/7/18.
+//  Copyright © 2018 worthbak. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
